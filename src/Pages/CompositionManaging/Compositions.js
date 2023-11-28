cfbb4156651207a948898a2196a87d9f80181640
S1OLY <shubaniyer@gmail.com>
2023-11-27 19:07:08 -0500

    Created PDF Addition
    Need to make specfic pdfs based on taals

--- src/Pages/CompositionManaging/Compositions.js
+++ src/Pages/CompositionManaging/Compositions.js
@@ -1,12 +1,11 @@
 // Compositions.js
 import React from 'react';
 import { useCompositions } from './Contexts/CompositionContextProvider';
+import pdf1 from '/Users/s1oly/Desktop/tablanote/src/PDFs/First_PDF.pdf'
 
 //Fix the display
 const Compositions = () => {
   const {compositions, deleteComposition} = useCompositions();
-
- 
 
   return (
     <>
@@ -15,7 +14,12 @@ const Compositions = () => {
         <ul>
           {compositions.map((composition, index) => (
           <>
-            <li key={index}>{composition} &nbsp; <button onClick = {() => deleteComposition(index)}> Delete Composition</button> </li>
+            <li key={index}>{composition} &nbsp; 
+              <button onClick = {() => deleteComposition(index)}> Delete Composition</button> 
+              <p>
+                <a href= {pdf1} target = "_blank" rel="nonrefferer"> Link To Composition</a>
+              </p>
+            </li>
            </>
           ))}
         </ul>

18ef0815baf8429673fc678f6eeb2a577dfb43e1
Shuban Iyer <90860963+s1oly@users.noreply.github.com>
2023-12-04 23:08:32 -0500

    Finalized Pop-Up box
    Add color, more images next

--- src/Pages/PopUp/Popup.js
+++ src/Pages/PopUp/Popup.js
@@ -1,21 +1,23 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import './Popup.css';
 
 const Popup = () => {
     const [isOpen, setIsOpen] = useState(true);
+
     return (
-        <>
-        {isOpen && (
-            <div>
+        <div className={`Popup ${isOpen ? '' : 'closed'}`}>
+            {isOpen && (
                 <div>
-                    <b>Welcome to TablaNote, a digital composition storage for the Tabla!</b>
+                    <div>
+                        <b>Welcome to TablaNote, a digital composition storage for the Tabla!</b>
+                    </div>
+                    <button onClick={() => setIsOpen(false)}>
+                        Close
+                    </button>
                 </div>
-                <button onClick={() => setIsOpen(false)}> 
-                    Close
-                </button>
-            </div>
-        )}
-        </>
+            )}
+        </div>
     );
-}
+};
 
 export default Popup;

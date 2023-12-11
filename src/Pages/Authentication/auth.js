7f526a5cc3d14bc50e469423aac63ee29b6e18cb
S1OLY <shubaniyer@gmail.com>
2023-12-10 23:22:25 -0500

    Added Google Sign In + Log Out

--- src/Pages/Authentication/auth.js
+++ src/Pages/Authentication/auth.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import {auth} from '../../Config/firebaseConfig'
-import {createUserWithEmailAndPassword} from 'firebase/auth'
+import {auth, googleProvider } from '../../Config/firebaseConfig'
+import {createUserWithEmailAndPassword, signInWithPopup, signOut} from 'firebase/auth'
 
 
 const Auth = () => {
@@ -9,14 +9,42 @@ const Auth = () => {
     const [password, SetPassword] = useState(" ");
 
     const SignIn = async () => {
+        try {
         await createUserWithEmailAndPassword(auth, email, password)
+        } catch (err) {
+            console.error(err)
+        }
+        
+    };
+
+    const googleSignIn = async () => {
+        try {
+        await signInWithPopup(auth, googleProvider)
+        } catch (err) {
+            console.error(err)
+        }
+        
+    };
+
+    const logout = async() => {
+        try {
+            await signOut (auth)
+            } catch (err) {
+                console.error(err)
+            }
     };
 
     return(
         <div>
             <input placeholder="Email" onChange={(e) => SetEmail(e.target.value)}/>
+            <p/>
             <input placeholder="Password" type = 'password' onChange={(e) => SetPassword(e.target.value)}/>
-            <button onClick={SignIn} >Sign In</button>
+            <p/>
+            <button onClick={SignIn}>Sign In</button>
+            <p/>
+            <button onClick = {googleSignIn}>Sign in with Google</button>
+            <p/>
+            <button onClick = {logout}>Log Out</button>
 
         </div>
     )

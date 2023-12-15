e73c05155f780113ecfc7659b857264592641ea2
Shuban Iyer <90860963+s1oly@users.noreply.github.com>
2023-12-15 13:29:09 -0500

    Slight Debug

--- src/Config/firebaseConfig.js
+++ src/Config/firebaseConfig.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import {GoogleAuthProvider} from 'firebase/auth'
+import {getAuth, GoogleAuthProvider} from 'firebase/auth'
 import {getFirestore} from 'firebase/firestore'
 import {getStorage} from 'firebase/storage'
 // TODO: Add SDKs for Firebase products that you want to use

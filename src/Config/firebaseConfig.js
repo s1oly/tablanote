12376925075f4ac126314601d496debc470ae26f
Shuban Iyer <90860963+s1oly@users.noreply.github.com>
2023-12-15 13:26:29 -0500

    Trying Netlify

--- src/Config/firebaseConfig.js
+++ src/Config/firebaseConfig.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getAuth, GoogleAuthProvider} from 'firebase/auth'
+import {GoogleAuthProvider} from 'firebase/auth'
 import {getFirestore} from 'firebase/firestore'
 import {getStorage} from 'firebase/storage'
 // TODO: Add SDKs for Firebase products that you want to use
@@ -25,5 +25,3 @@ export const auth = getAuth(app);
 export const googleProvider= new GoogleAuthProvider();
 export const db= getFirestore(app);
 export const storage = getStorage(app);
-
-const analytics = getAnalytics(app);

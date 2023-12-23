2bede073e3765d95b6a510e2163223e77f29a410
Shuban Iyer <90860963+s1oly@users.noreply.github.com>
2023-12-23 17:45:14 -0500

    Changed Email

--- src/Pages/Display/About.js
+++ src/Pages/Display/About.js
@@ -10,7 +10,7 @@ const About = () =>{
             <p>
                 This project came about as an idea I had while looking for a Kayeda while practicing tabla. I looked through my paper notes, however due to the lack of organization
                 I wasn't able to find it. I came up with the idea of a digital storage for different tabla compositions, whether it be a Kayeda or Rela. Currently, TablaNote only supports 
-                Teentaal,Jhaptaal,Ektaal, and Rupak Compositions of one Awartan, however we look to expand on that on the future. If you have any questions, please email tablanote@gmail.com.
+                Teentaal, Jhaptaal, Ektaal, and Rupak Compositions of one Awartan, however we look to expand on that on the future. If you have any questions, please email shubaniyer1@gmail.com.
             </p>
             <img src = {image} alt='Why is this blank'/>
         </div>
